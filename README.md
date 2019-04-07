8f7b8d0273ab1be6fa6307f6861f930bc5d679bf
Syed Furqan Ahmed <sonicfurqan@gmail.com>
2019-04-07 18:40:24 +0530

    Ace Editor support

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
-# LWC-Viewer
+# LWC-Editor
 Lightweigh tab to view and edit LWC Component
 
-copy code or use https://login.salesforce.com/packaging/installPackage.apexp?p0=04t0o0000020D62
+copy code or install unmanaged package from here  https://syeds-lwc-editor-developer-edition.ap0.force.com/
 to add package to your org
+
+LWC Editor app contains editor tab for editing and creating components
 
 How to use
 
-1.Add your org domain to remote site settings as it uses tooling api to fetch data
+1.Add your org domain to remote site settings as it uses tooling api to fetch data (Get Domain name by executing 
 
-2.search for tab "view all components "
+URL.getSalesforceBaseUrl().toExternalForm() )
 
-3.Enjoy
+2.Update your domain name to custom label 'DomainName' if you want to use editor option 
+
+a.If click jack protoection is enablend in your org add vf page and lighting urls to whitelisting by going to setup>sessionsetings>add whitlist
+
+3.toggle editor by using Switch TO Editor
+
+4.toggel editor full screen by using Show File Section
+
+5.Editor Shortcut 
+
+a.CNTRL+S=saves file 
+
+b.SHIFT+TAB=Prettify code 
+
+c.CNTRL+Space=Show autocomplete code 
+
+d. Alt-Shift-F=Toggle Full screen
+
--- lwc/viewweb/viewweb.js
+++ lwc/viewweb/viewweb.js
@@ -1,41 +1,99 @@
-import { LightningElement, wire, track } from 'lwc';
+import {
+    LightningElement,
+    wire,
+    track
+} from 'lwc';
 import getLWCList from '@salesforce/apex/ViewComponents.getWebComponentList'
 import queryWebComponent from '@salesforce/apex/ViewComponents.getWebComponent'
 import updateWebComponent from '@salesforce/apex/ViewComponents.setWebComponent'
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import insertComponent from '@salesforce/apex/ViewComponents.createWebComponent'
+
+import DomainName from '@salesforce/label/c.DomainName';
+import {
+    ShowToastEvent
+} from 'lightning/platformShowToastEvent';
 export default class Viewweb extends LightningElement {
+    @track creatingFile=false;
     @track resultList;
     @track timestamp;
     @track countOfMetadata = 'LWC Components';
     @track isLoading = true;
     @track showResult;
-    @track saving=false;
+    @track saving = false;
+    @track ace = false;
+    @track istreeLoading = false;
+    @track showFile = true;
+    @track showpopup = false;
+    @track layout = 'slds-size_4-of-6';
+    @track fileName = '';
+    @track recordPage = false;
+    @track homepage = false;
+    @track allpage = false;
+
+
+    halfLayout = 'slds-size_4-of-6';
+    fullLayout = 'slds-size_6-of-6';
+
+
     containerForMetadataResult = [];
     containerForWebBundleResult = [];
-
-
-
+    orgnalresultList;
+    @track queryTerm;
+    sourceOfIframe = 'https://'+DomainName+'-dev-ed--c.visualforce.com';
 
     connectedCallback() {
+        let that = this;
+        window.addEventListener("message", function(event) {
+            if (event.origin !== that.sourceOfIframe) {
+                // Not the expected origin: Reject the message!
+                return;
+            }
+            // Handle the message
+            if (event.data !== undefined) {
+                let data = event.data;
+                if (data.type == 'code') {
+                    that.showResult = data.soruce;
+                }
+                if (data.type == 'save') {
+                    that.showResult = data.soruce;
+                    that.handlesaveClick();
+                }
+                if (data.type == 'fullscreen') {
+                    that.showFile = !that.showFile;
+                    if (that.showFile === true) {
+                        that.layout = that.halfLayout;
+                    }
+                    if (that.showFile === false) {
+                        that.layout = that.fullLayout;
+                    }
+                }
+
+            }
+        });
+
+
+
+
         this.isLoading = true;
         getLWCList().then(result => {
             let data = JSON.parse(result);
             if (data.records) {
                 let parsingData = [];
                 data.records.forEach(function (record) {
+                    let date = new Date(Date.parse(record.LastModifiedDate));
                     let oneRecord = {
                         label: record.MasterLabel,
                         name: record.Id,
-                        metatext: 'LastModified By ' + record.LastModifiedBy.Name + ',' + record.LastModifiedDate,
+                        metatext: 'LastModified By ' + record.LastModifiedBy.Name + ',' + date,
                         expanded: false,
                     }
                     parsingData.push(oneRecord);
                 });
                 this.countOfMetadata = 'LWC Components (' + data.records.length + ')';
                 this.resultList = parsingData;
+                this.orgnalresultList = parsingData;
                 this.containerForMetadataResult = data.records;
-            }
-            else {
+            } else {
                 this.countOfMetadata = 'LWC Components (0)';
             }
 
@@ -44,8 +102,120 @@ export default class Viewweb extends LightningElement {
             this.isLoading = false;
         })
 
+
+    }
+    connecttoIframe(type,
+        source) {
+
+        let iframe = this.template.querySelector('iframe');
+        if (iframe) {
+
+            let message = {
+                mode: type,
+                code: source
+            };
+
+            iframe.contentWindow.postMessage(message, this.sourceOfIframe);
+        }
+
     }
 
+    handleNewClick() {
+        let that = this;
+        let fileName = this.fileName;
+        if (!fileName.trim()) {
+            return;
+        }
+        if (fileName.trim().includes(' ')) {
+            const event = new ShowToastEvent({
+                title: 'Incorrect File Name',
+                message: 'File Name cannot have blank spaces',
+                variant: 'error',
+
+            });
+            this.dispatchEvent(event);
+            return;
+        }
+        console.log(this.homepage);
+        console.log(this.allpage);
+        console.log(this.recordPage);
+        this.creatingFile=true;
+        insertComponent({
+            fileName: fileName.trim(),
+            isRecordPage: this.recordPage,
+            ishomePage: this.homepage,
+            isallPage: this.allpage
+        }).then(result=> {
+            console.log(result)
+            if (result == 'true') {
+                that.connectedCallback();
+                const event = new ShowToastEvent({
+                    title: fileName+' Created',
+                    variant: 'success'
+                });
+                that.dispatchEvent(event);
+
+            } else {
+                const event = new ShowToastEvent({
+                    title: 'Error Createing LWC',
+                    message: result, 
+                    variant: 'error',
+                    mode: 'pester'
+                });
+                that.dispatchEvent(event);
+            }
+            that.showpopup = false;
+            that.creatingFile=false;
+        }).catch(error => {
+            console.log('error',
+                error);
+            that.showpopup = false;
+             that.creatingFile=false;
+        });
+
+    }
+    togglepopup() {
+
+        this.showpopup=!this.showpopup;
+
+    }
+    handleKeyUp(evt) {
+        const isEnterKey = evt.keyCode === 13;
+        let searchKey = evt.target.value;
+        if (isEnterKey) {
+            let result = this.orgnalresultList.filter(data => data.label.toLowerCase().includes(searchKey.toLowerCase()));
+            this.resultList = result;
+
+        }
+        if (!searchKey) {
+            this.resultList = this.orgnalresultList;
+        }
+    }
+    updateValue(event) {
+        const fieldName = event.target.name;
+        if (fieldName == 'fileName') {
+            this.fileName = event.target.value;
+        } else if (fieldName == 'home') {
+            this.homepage = event.target.checked;
+        } else if (fieldName == 'allp') {
+            this.allpage = event.target.checked;
+        } else if (fieldName == 'recod') {
+            this.recordPage = event.target.checked;
+        }
+    }
+    swichEditor(evt) {
+        this.ace = evt.target.checked;
+    }
+
+    toggleFiles(evt) {
+        this.showFile = evt.target.checked;
+        if (this.showFile === true) {
+            this.layout = this.halfLayout;
+        }
+        if (this.showFile === false) {
+            this.layout = this.fullLayout;
+        }
+    }
 
     handleCodeEvent(event) {
         this.showResult = event.target.value;
@@ -57,7 +227,9 @@ export default class Viewweb extends LightningElement {
 
         if (recordData.length > 0) {
             if (recordData[0].attributes.type === 'LightningComponentBundle') {
-                this.isLoading = true;
+                this.isLoading = !this.ace;
+                if (this.isLoading == false)
+                    this.istreeLoading = true;
                 queryWebComponent({
                     id: selectedId
                 }).then(result => {
@@ -93,35 +265,42 @@ export default class Viewweb extends LightningElement {
                     });
                     this.resultList = resultRefresh;
                     this.isLoading = false;
+                    this.istreeLoading = false;
                 }).catch(error => {
-                    console.log('error', error);
+                    console.log('error',
+                        error);
                 });
             }
-        }
-        else {
-            this.isLoading = true;
+        } else {
+            this.isLoading = !this.ace;
             let webData = this.containerForWebBundleResult.filter(a => a.Id === selectedId);
             if (webData.length > 0) {
+
+
+                let date = new Date(Date.parse(webData[0].LastModifiedDate));
                 this.showResult = webData[0].Source;
                 this.continerForDispalyedResult = webData[0];
-                this.timestamp = 'LastModified By ' + webData[0].LastModifiedBy.Name + ',' + webData[0].LastModifiedDate;
+                this.timestamp = 'LastModified By ' + webData[0].LastModifiedBy.Name + ',' + date;
                 this.isUpdateAvilabel = true;
+                this.connecttoIframe(webData[0].Format, webData[0].Source);
             }
-            setTimeout(function () { that.isLoading = false; }, 100);
+            setTimeout(function () {
+                that.isLoading = false;
+            }, 100);
         }
     }
-    handlesaveClick(event) {
-        let that=this;
+    handlesaveClick() {
+        let that = this;
         let bundle = this.containerForWebBundleResult;
         let data = this.continerForDispalyedResult;
         let id = data.Id;
         let source = this.showResult;
-        console.log(source);
-        console.log(id);
+        //        console.log(source);
+        //    console.log(id);
         let jsonString = JSON.stringify(source);
-        console.log(jsonString);
-        if (source && id){
-            this.saving=true;
+        //  console.log(jsonString);
+        if (source && id) {
+            this.saving = true;
             updateWebComponent({
                 id: id,
                 sourceString: jsonString
@@ -130,17 +309,20 @@ export default class Viewweb extends LightningElement {
 
                     let data = JSON.parse(result);
                     if (data.length > 0 && data[0].errorCode) {
+                        console.error(data[0].errorCode);
                         const event = new ShowToastEvent({
                             title: 'Error cannot save ' + data[0].errorCode,
-                            message: data[0].message
+                            message: data[0].message,
+                            variant: 'error',
+                            mode: 'pester'
                         });
                         this.dispatchEvent(event);
-                        this.saving=false;
+                        this.saving = false;
                         return
                     }
-                    this.saving=false;
+                    this.saving = false;
                 }
-              
+
                 bundle.forEach(function (record) {
                     if (record.Id === id) {
                         record.Source = source;
@@ -148,19 +330,22 @@ export default class Viewweb extends LightningElement {
                 });
                 this.containerForWebBundleResult = bundle;
                 const event = new ShowToastEvent({
-                    title: 'Saved'
+                    title: 'Saved',
+                    variant: 'success'
                 });
                 this.dispatchEvent(event);
-                this.saving=false;
+                this.saving = false;
             }).catch(error => {
+                console.error(error);
                 const event = new ShowToastEvent({
-                    title: 'Error cannot save'
+                    title: 'Error cannot save',
+                    variant: 'error'
                 });
                 this.dispatchEvent(event);
-                this.saving=false;
+                this.saving = false;
             })
         }
-            
+
 
     }
 }
